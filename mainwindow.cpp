4eadf74c3ec8648e618a4c5503ac67876ddc3149
stepan.rutz <stepan.rutz@gmx.de>
2025-02-08 19:19:44 +0100

    initial

---
+++ mainwindow.cpp
@@ -0,0 +1,51 @@
+
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QJsonObject>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QTextBrowser>
+#include <QLabel>
+#include <QDebug>
+#include <QScrollArea>
+#include <QSplitter>
+#include <QMargins>
+#include <QFontDatabase>
+#include <future>
+#include <memory>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    auto layout = new QVBoxLayout(this->centralWidget());
+
+    auto splitter = new QSplitter(Qt::Horizontal, this);
+    layout->addWidget(splitter, 1);
+
+    auto textBrowser = new QTextBrowser(this);
+    auto fixedFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
+    fixedFont.setPointSize(9);
+    textBrowser->setFont(fixedFont);
+    splitter->addWidget(textBrowser);
+
+    auto dataTable = new QLabel("test");
+    splitter->addWidget(dataTable);
+
+    QList<int> sizes;
+    sizes << 200 << 300;
+    splitter->setSizes(sizes);
+
+    auto buttonsPanel = new QWidget(this);
+    auto buttonsPanelLayout = new QHBoxLayout(buttonsPanel);
+    buttonsPanelLayout->addStretch();
+    buttonsPanelLayout->setContentsMargins(QMargins(0, 0, 0, 0));
+    layout->addWidget(buttonsPanel);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
